ab97481ecd9001c423669e40fc3b5e6f24de730c
JohnIanOngayi <ongayijohnian@gmail.com>
2024-12-13 07:52:54 +0300

    Number Data Types

--- Program.cs
+++ Program.cs
@@ -5,7 +5,28 @@ namespace HelloWorld
     {
             static void Main(string[] args)
             {
-                    Console.WriteLine("Hello World!");
+                    int age = 23;
+                    Console.WriteLine(age);
+                    Console.WriteLine(int.MaxValue);
+                    Console.WriteLine(int.MinValue);
+                    Console.WriteLine();
+
+                    long bigNumber = 900000000000L;
+                    Console.WriteLine(bigNumber);
+                    Console.WriteLine(long.MaxValue);
+                    Console.WriteLine(long.MinValue);
+                    Console.WriteLine();
+
+                    double negative = -44.5D;
+                    Console.WriteLine(negative);
+                    Console.WriteLine(double.MaxValue);
+                    Console.WriteLine(double.MinValue);
+                    Console.WriteLine();
+
+                    decimal average = 14.99M;
+                    float gpa = 3.5F;
+                    Console.WriteLine(average);
+                    Console.WriteLine(gpa);
             }
     }
 }

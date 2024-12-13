30f03c55c35d0e01d4cd828dc4cc098e84d227cd
JohnIanOngayi <ongayijohnian@gmail.com>
2024-12-13 13:29:11 +0300

    Numeric Formatting

--- Program.cs
+++ Program.cs
@@ -5,20 +5,23 @@ namespace HelloWorld
         {
                 static void Main(string[] args)
                 {
-                        Console.Write("How many prints: ");
-                        string? countInput = Console.ReadLine();
+                        double value = 1000D / 12.34D;
+                        Console.WriteLine($"{value}");     // default
+                        Console.WriteLine($"{value:0.0}"); // 1dp
+                        Console.WriteLine($"{value:0.#}"); // Remove trailing 0s
+                        Console.WriteLine($"{value:0.00}");// 2dp
 
-                        int count = Convert.ToInt32(countInput);
+                        double money = 10D / 3D;
+                        double negativeMoney = -10D / 3D;
+                        Console.WriteLine(money);
 
-                        if (count <= 0)
-                        {
-                                Console.WriteLine("Please enter a positive integer");
-                                return;
-                        }
-                        for (int i = 1; i <= count; i++)
-                        {
-                            Console.WriteLine($"Print: {i}");
-                        }
+                        // c# can format numeric input to a currency
+                        Console.WriteLine($"Currency Money: {money.ToString("C")}");  // Currency default
+                        Console.WriteLine($"Currency Money: {money.ToString("C1")}"); // Currency to 1dp
+                        Console.WriteLine($"Currency Money: {money.ToString("C2")}"); // Currency to 2dp
+
+                        // c# will even know to put negative sign before symbol
+                        Console.WriteLine($"Negative Money: {negativeMoney.ToString("C")}");
                 }
         }
 }

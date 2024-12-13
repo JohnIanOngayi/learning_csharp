bfee431c240e370141d801151a0344ea4cb5c275
JohnIanOngayi <ongayijohnian@gmail.com>
2024-12-13 13:59:25 +0300

    TryParse for more elegant conversions

--- Program.cs
+++ Program.cs
@@ -1,29 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System.Globalization;
 namespace HelloWorld
 {
         class Program
         {
                 static void Main(string[] args)
                 {
-                        double value = 1000D / 12.34D;
-                        Console.WriteLine($"{value}");     // default
-                        Console.WriteLine($"{value:0.0}"); // 1dp
-                        Console.WriteLine($"{value:0.#}"); // Remove trailing 0s
-                        Console.WriteLine($"{value:0.00}");// 2dp
+                        Console.Write("Enter Your Age: ");
+                        string? ageInput = Console.ReadLine();
 
-                        double money = 10D / 3D;
-                        double negativeMoney = -10D / 3D;
-                        Console.WriteLine(money);
-                        Console.WriteLine(negativeMoney);
-
-                        // Get my currency
-                        Console.WriteLine(money.ToString("C", CultureInfo.CurrentCulture));
-
-                        // Set the currency
-                        Console.WriteLine(money.ToString("C", CultureInfo.CreateSpecificCulture("en-GB")));
-                        Console.WriteLine(money.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
-                        Console.WriteLine(negativeMoney.ToString("C", CultureInfo.CreateSpecificCulture("en-AU")));
+                        int age;
+                        bool success = int.TryParse(ageInput, out age);
+                        if (success) Console.WriteLine(age);
+                        else Console.WriteLine("Invalid: Age can only be a number");
                 }
         }
 }

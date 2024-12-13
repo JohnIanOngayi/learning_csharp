2d569fc52025de7f9522e436864d250e7ec0681c
JohnIanOngayi <ongayijohnian@gmail.com>
2024-12-13 12:57:08 +0300

    Loops

--- Program.cs
+++ Program.cs
@@ -5,35 +5,20 @@ namespace HelloWorld
         {
                 static void Main(string[] args)
                 {
-                        Console.WriteLine("Hello, Welcome!");
-                        Console.WriteLine();
+                        Console.Write("How many prints: ");
+                        string? countInput = Console.ReadLine();
 
-                        Console.Write("Enter day of the week:(1 - 7) ");
-                        string? dayInput = Console.ReadLine();
-                        int dayNum = Convert.ToInt32(dayInput);
-                        string day;
+                        int count = Convert.ToInt32(countInput);
 
-                        switch (dayNum)
+                        if (count <= 0)
                         {
-                                case 0:
-                                case 1: day = "Monday";
-                                        break;
-                                case 2: day = "Tuesday";
-                                        break;
-                                case 3: day = "Wednesday";
-                                        break;
-                                case 4: day = "Thursday";
-                                        break;
-                                case 5: day = "Friday";
-                                        break;
-                                case 6: day = "Saturday";
-                                        break;
-                                case 7: day = "Sunday";
-                                        break;
-                                default: day = "Invalid!";
-                                         break;
+                                Console.WriteLine("Please enter a positive integer");
+                                return;
                         }
-                        Console.WriteLine($"Day: {day}");
+                        for (int i = 1; i <= count; i++)
+                        {
+                            Console.WriteLine($"Print: {i}");
+                        }
                 }
         }
 }

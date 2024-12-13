17ba7c815b3a226d29f3085452c87038047e4af3
JohnIanOngayi <ongayijohnian@gmail.com>
2024-12-13 12:45:36 +0300

    switch case

--- Program.cs
+++ Program.cs
@@ -1,21 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
 namespace HelloWorld
 {
-    class Program
-    {
-            static void Main(string[] args)
-            {
-                    Console.WriteLine("Hello, Welcome!");
+        class Program
+        {
+                static void Main(string[] args)
+                {
+                        Console.WriteLine("Hello, Welcome!");
+                        Console.WriteLine();
 
-                    Console.Write("Enter Your Name: ");
-                    string? name = Console.ReadLine();
-                    Console.Write("Enter Your Age: ");
-                    string? ageInput = Console.ReadLine();
-                    int age = Convert.ToInt32(ageInput);
-                    Console.Write($"{name} - {age}");
+                        Console.Write("Enter day of the week:(1 - 7) ");
+                        string? dayInput = Console.ReadLine();
+                        int dayNum = Convert.ToInt32(dayInput);
+                        string day;
 
-                    if (age >= 18) Console.WriteLine(" - Entry Granted!");
-                    else Console.WriteLine(" - Entry Denied!");
-            }
-    }
+                        switch (dayNum)
+                        {
+                                case 0:
+                                case 1: day = "Monday";
+                                        break;
+                                case 2: day = "Tuesday";
+                                        break;
+                                case 3: day = "Wednesday";
+                                        break;
+                                case 4: day = "Thursday";
+                                        break;
+                                case 5: day = "Friday";
+                                        break;
+                                case 6: day = "Saturday";
+                                        break;
+                                case 7: day = "Sunday";
+                                        break;
+                                default: day = "Invalid!";
+                                         break;
+                        }
+                        Console.WriteLine($"Day: {day}");
+                }
+        }
 }

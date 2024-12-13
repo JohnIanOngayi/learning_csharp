1b9502c54910d23540f7f91b77679387b575d979
JohnIanOngayi <ongayijohnian@gmail.com>
2024-12-13 14:16:19 +0300

    Multiplication Table

--- Program.cs
+++ Program.cs
@@ -5,13 +5,29 @@ namespace HelloWorld
         {
                 static void Main(string[] args)
                 {
-                        Console.Write("Enter Your Age: ");
-                        string? ageInput = Console.ReadLine();
+                        /** ASk the  user for the number
+                         * Prnit a times table
+                         */
+                        Console.WriteLine("Multiplications Table");
+                        Console.WriteLine("=====================");
+                        Console.Write("Enter Number: ");
+                        string? numInput = Console.ReadLine();
 
-                        int age;
-                        bool success = int.TryParse(ageInput, out age);
-                        if (success) Console.WriteLine(age);
-                        else Console.WriteLine("Invalid: Age can only be a number");
+                        bool success = int.TryParse(numInput, out int num);
+                        if (!success || num < 0)
+                        {
+                                Console.WriteLine("Enter a Valid Number");
+                                return;
+                        }
+                        for (int i = 0; i <= num; i++)
+                        {
+                                for (int j = 0; j <= num; j++)
+                                {
+                                        int product = i * j;
+                                        Console.Write($" {product:000.}");
+                                }
+                                Console.WriteLine();
+                        }
                 }
         }
 }
